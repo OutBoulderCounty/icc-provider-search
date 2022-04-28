e5eaed35c4a356caceabd891a8aae1236a37086d
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2022-04-28 10:19:08 -0700

    added feature for filtering providers

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
     null
   )
 
+  //Modal diplays provider information when clicked
   const displayModal = (provider: any) => {
     setIsModalOpen(true)
     if (data) {
@@ -29,20 +30,56 @@ const Home: NextPage = () => {
     }
   }
 
+  //Search input functionality
   const search = (e: React.SyntheticEvent) => {
     e.preventDefault()
     const target = e.target as typeof e.target & {
       value: string
     }
     console.log({ target })
+
     if (data) {
-      setFilteredProviders(
-        data.filter((provider) => {
-          return provider.providers__name
-            .toLowerCase()
-            .includes(target.value.toLowerCase())
-        })
-      )
+      // setFilteredProviders(
+      //   data.filter((provider) => {
+      //       return provider.providers__name
+      //         .toLowerCase()
+      //         .includes(target.value.toLowerCase())
+      //   })
+      // )
+
+      //match typed words to any of the attributes
+      const results = data.filter((provider) => {
+        if(provider.providers__name.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__pronouns?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__practice_name.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__practice_nick_name?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__address?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__specialty_of_practice_focus?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__disability_accomodations?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__language_support?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        if(provider.providers__email?.toLowerCase().includes(target.value.toLowerCase())){
+          return provider
+        }
+        
+      })
+      // console.log(results)
+      setFilteredProviders(results)
     }
   }
 
@@ -53,9 +90,30 @@ const Home: NextPage = () => {
           <NavBar />
         </header>
         {error ? <Error name={error.name} message={error.message} /> : null}
-        <form onChange={search}>
-          <input type="text" name="search" id="search" placeholder="Search" />
-        </form>
+
+        <div className="w-full sm:max-w-xs md:max-w-3xl mt-5 mx-auto">
+          <form
+            onChange={search}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Find a provider
+              </label>
+              <input
+                className="shadow appearance-none border rounded sm:w-72 md:w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline block"
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search"
+              />
+            </div>
+          </form>
+        </div>
+
         <ul
           role="list"
           className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 m-10"
